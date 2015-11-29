865975b64fc5790c10d9e2b879b5f54ecd3d2c63
Loren <Loren.sarthur@gmail.com>
2015-11-29 12:59:46 -0800

    Initial Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/loren/Workspaces/final_catkin/src
+# Build directory: /home/loren/Workspaces/final_catkin/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(simple_navigation_goals)
+SUBDIRS(frontier_exploration)
+SUBDIRS(final_project)
+SUBDIRS(nav_bundle)
+SUBDIRS(stagebot_2dnav)
