491cdbd8a92ab9aab61ce76abdd3340bd398c0cb
Loren <Loren.sarthur@gmail.com>
2015-11-29 17:47:20 -0800

    Exploration publisher

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,8 +5,17 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 SUBDIRS(gtest)
+SUBDIRS(hector_navigation/hector_elevation_msgs)
+SUBDIRS(hector_navigation/hector_driving_aid_markers)
 SUBDIRS(simple_navigation_goals)
+SUBDIRS(hector_navigation/hector_elevation_visualization)
+SUBDIRS(hector_navigation/hector_path_follower)
+SUBDIRS(hector_navigation/hector_exploration_controller)
+SUBDIRS(hector_navigation/hector_costmap)
+SUBDIRS(hector_navigation/hector_elevation_mapping)
 SUBDIRS(frontier_exploration)
+SUBDIRS(hector_navigation/hector_exploration_planner)
+SUBDIRS(hector_navigation/hector_exploration_node)
 SUBDIRS(final_project)
 SUBDIRS(nav_bundle)
 SUBDIRS(stagebot_2dnav)
